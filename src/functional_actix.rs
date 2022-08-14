fc5ac9563b6a25d4196ee9f310a79cbd4c7ff127
Mikayla Maki <mikayla.c.maki@icloud.com>
2022-08-13 21:41:00 -0700

    Restarting with dissertation

--- src/functional_actix.rs
+++ src/functional_actix.rs
@@ -4,17 +4,17 @@ use actix::dev::MessageResponse;
 use actix::Message;
 use actix::{Actor, Context, Handler};
 
-///A basic message
-pub struct FunctionMsg<A, R> {
+//A message to call a function with the matching argument and return type
+pub struct FunctionCall<A, R> {
     args: A,
     _marker: PhantomData<R>,
 }
 
-impl<A, R: 'static> Message for FunctionMsg<A, R> {
+impl<A, R: 'static> Message for FunctionCall<A, R> {
     type Result = R;
 }
 
-impl<A, R> FunctionMsg<A, R> {
+impl<A, R> FunctionCall<A, R> {
     pub fn new(a: A) -> Self {
         Self {
             args: a,
@@ -23,35 +23,59 @@ impl<A, R> FunctionMsg<A, R> {
     }
 }
 
-///An actor that can respond to this message
+///An actor that will store and execute a given closure
 pub struct FunctionActor<A, R> {
     closure: Box<dyn FnMut(A) -> R>,
-    _arg_marker: PhantomData<A>,
-    _res_marker: PhantomData<R>,
 }
 
 impl<A, R> FunctionActor<A, R> {
     pub fn new(f: Box<dyn FnMut(A) -> R>) -> Self {
-        Self {
-            closure: f,
-            _arg_marker: PhantomData,
-            _res_marker: PhantomData,
-        }
+        Self { closure: f }
     }
 }
 
-impl<A: Unpin + 'static, R: Unpin + 'static> Actor for FunctionActor<A, R> {
+impl<A, R> Actor for FunctionActor<A, R>
+where
+    A: Unpin + 'static,
+    R: Unpin + 'static,
+{
     type Context = Context<Self>;
 }
 
-impl<A, R> Handler<FunctionMsg<A, R>> for FunctionActor<A, R>
+impl<A, R> Handler<FunctionCall<A, R>> for FunctionActor<A, R>
 where
     A: Unpin + 'static,
-    R: Unpin + MessageResponse<FunctionActor<A, R>, FunctionMsg<A, R>> + 'static,
+    R: Unpin + MessageResponse<FunctionActor<A, R>, FunctionCall<A, R>> + 'static,
 {
     type Result = R;
 
-    fn handle(&mut self, msg: FunctionMsg<A, R>, _ctx: &mut Self::Context) -> Self::Result {
-        (*self.closure)(msg.args)
+    fn handle(&mut self, msg: FunctionCall<A, R>, _ctx: &mut Self::Context) -> Self::Result {
+        (self.closure)(msg.args)
     }
 }
+
+//Could also implement this as pure dynamic dispatch... but that makes me sad :(
+// struct CallMethod1<A1, R1> {
+//     msg: FunctionCall<A1, R1>,
+// }
+
+// struct CallMethod2<A2, R2> {
+//     msg: FunctionCall<A2, R2>,
+// }
+
+// struct MethodActor<A1, R1, A2, R2> {
+//     fn_1: FunctionActor<A1, R1>,
+//     fn_2: FunctionActor<A2, R2>,
+// }
+
+// impl<A1, R1, A2, R2> Actor for MethodActor<A1, R1, A2, R2>
+// where
+//     A1: Unpin + 'static,
+//     R1: Unpin + 'static,
+//     A2: Unpin + 'static,
+//     R2: Unpin + 'static,
+// {
+//     type Context = Context<Self>;
+// }
+
+// impl<A, R> Handler for MethodActor<>
--- src/main.rs
+++ src/main.rs
@@ -1,27 +1,39 @@
 use actix::Actor;
 use capabilities::Become;
 
-use crate::functional_actix::{FunctionActor, FunctionMsg};
+use crate::functional_actix::{FunctionActor, FunctionCall};
 
 mod capabilities;
 mod functional_actix;
 
 #[actix::main]
 async fn main() {
-    let gary_exec = FunctionActor::new(greeter(Become, "Gary".to_string())).start();
-    let res = gary_exec
-        .send(FunctionMsg::new(GreeterArgs {
-            your_name: "Alice".to_string(),
-        }))
-        .await;
+    call_greet().await;
+    cell_chest().await;
+}
+
+//Spritely Goblins tutorial examples
+
+//Greeter:
+async fn call_greet() {
+    let gary_fn = FunctionActor::new(greeter(Become, "Gary".to_string())).start();
+    let res = gary_fn.send(FunctionCall::new("Alice".to_string())).await;
 
     println!("{:?}", res);
 }
 
-struct GreeterArgs {
-    your_name: String,
+fn greeter(_capability: Become, our_name: String) -> Box<dyn FnMut(String) -> String> {
+    Box::new(move |your_name| format!("Hello {}, my name is {}", your_name, our_name))
 }
 
-fn greeter(_capability: Become, our_name: String) -> Box<dyn FnMut(GreeterArgs) -> String> {
-    Box::new(move |args: GreeterArgs| format!("Hello {}, my name is {}", args.your_name, our_name))
+//Cell greeter
+async fn cell_chest() {
+    let gary_fn = FunctionActor::new(cell_greeter(Become, "Gary".to_string())).start();
+    let res = gary_fn.send(FunctionCall::new("Alice".to_string())).await;
+
+    println!("cell {:?}", res);
+}
+
+fn cell_greeter(_capability: Become, our_name: String) -> Box<dyn FnMut(String) -> String> {
+    Box::new(move |your_name| format!("Hello {}, my name is {}", your_name, our_name))
 }
